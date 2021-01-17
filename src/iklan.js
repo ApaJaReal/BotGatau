51398c9921f4cf540dd6984c26222e1daeec3158
ApaJaReal <71695977+ApaJaReal@users.noreply.github.com>
2021-01-17 14:12:38 +0700

    Add files via upload

---
+++ src/iklan.js
@@ -0,0 +1,29 @@
+const iklan = () => { 
+	return `           
+╔══✪〘 IKLAN 〙✪══
+║
+╠═══════════════════════════
+╠➥ *DAFTAR SEWA & BUAT BOT :*
+╠➥ *SEWA : 10K/GRUP (BULAN)*
+╠➥ *BUAT : 35K (BISA JADI OWNER)*
+╠➥ *PEMBAYARAN BISA MELALUI :*
+╠➥ *PULSA,SAWERIA*
+╠═══════════════════════════
+╠➥ *KEUNTUNGAN SEWA BOT :*
+╠➥ *1. BISA MENJADI ADMIN YoGi*
+╠➥ *2. BISA MENDAPATKAN COMMAND ADMIN*
+╠➥ *KEUNTUNGAN BUAT BOT :*
+╠➥ *1. BISA MENJADI OWNER BOT SENDIRI*
+╠➥ *2. BISA MENGGANTI NAMA BOT SENDIRI*
+╠➥ *3. BISA MEMBAWA BOT KE GROUP*
+╠➥ *4. BISA MENGGUNAKAN COMMAND OWNER*
+╠➥ *5. BISA MENYEWAKAN BOT KEMBALI*
+╠═══════════════════════════
+╠➥ *JIKA MINAT IKLAN DIATAS*
+╠➥ *HARAP HUBUNGI NOMOR DIBAWAH :*
+╠➥ *wa.me/6285784952920*
+║
+╚═〘 BoT-YoGi 〙
+`
+}
+exports.iklan = iklan
